92c5b7b2bf49c19a3a6a1ed5fed8553c1c8ec561
Matheus Sales <157637433+devmatheussales@users.noreply.github.com>
2024-02-05 07:21:18 -0300

    'desafio'
    transformar o ex009 em um site para interagir com o usuario

---
+++ aula11/ex008.js
@@ -0,0 +1,10 @@
+//console.log('Olá Mundo!')
+
+var velocidade = 60
+console.log(`A velocidade do seu carro é ${velocidade} KM/h`)
+
+if (velocidade > 60) { // Condição simples!
+    console.log('Você ultrapassou a velocidade permitida.')
+
+}
+console.log('Dirija sempre usando cinto de segurança!')
