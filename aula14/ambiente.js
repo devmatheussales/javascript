74c07ca8bfe0e298c7742a6f569a1121f455193c
Matheus Sales <157637433+devmatheussales@users.noreply.github.com>
2024-02-07 14:40:40 -0300

    Create ambiente.js
    Repetições parte2

---
+++ aula14/ambiente.js
@@ -0,0 +1,14 @@
+// var contador = 1
+// while ( contador <= 5){
+//     console.log(contador)
+//     contador++
+// }
+
+ //----------------
+
+
+ console.log('Vai começar...')
+for (var contador =1; contador <= 4; contador++) {
+    console.log(contador)
+}
+console.log('FIM')
