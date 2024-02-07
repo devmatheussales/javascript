dd5f32eaca51d04233a4f332abb54449933ba3ab
Matheus Sales <157637433+devmatheussales@users.noreply.github.com>
2024-02-07 16:53:26 -0300

    ex017
    tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,21 @@
-alert('Aoba!')
+ function tabuada() {
+    let num = document.getElementById('num')
+    let tab = document.getElementById('seltab')
+
+    if (num.value.length == 0){
+
+        window.alert('falta o numero')
+    }
+    else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        for (c <= 10; c <=10; c ++ ){
+            let item = document.createElement('option')
+            item.text =` ${n} * ${c} = ${n*c}` 
+            item.value = `tab ${c}`
+            tab.appendChild(item)
+        }
+    }
+
+ }
