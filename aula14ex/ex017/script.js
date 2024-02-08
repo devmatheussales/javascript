0b4bacacfe60c063c03b66a225da2f2f6b450d65
Matheus Sales <157637433+devmatheussales@users.noreply.github.com>
2024-02-08 07:41:31 -0300

    Update script.js

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,8 +1,8 @@
- function tabuada() {
+function tabuada() {
     let num = document.getElementById('num')
     let tab = document.getElementById('seltab')
 
-    if (num.value.length == 0){
+    if (num.value.length == 0) {
 
         window.alert('falta o numero')
     }
@@ -10,12 +10,12 @@
         let n = Number(num.value)
         let c = 1
         tab.innerHTML = ''
-        for (c <= 10; c <=10; c ++ ){
+        for (c <= 10; c <= 10; c++) {
             let item = document.createElement('option')
-            item.text =` ${n} * ${c} = ${n*c}` 
+            item.text = ` ${n} * ${c} = ${n * c}`
             item.value = `tab ${c}`
             tab.appendChild(item)
         }
     }
 
- }
+}
